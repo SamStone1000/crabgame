536836750e29cd423a05a17deb4f7d94953d8189
Stev51 <87919966+Stev51@users.noreply.github.com>
2022-05-19 10:58:56 -0500

    multifile 1

---
+++ button_new.js
@@ -0,0 +1,5 @@
+function bNewFunc(){
+    let labCrab = new Crab([1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],[1,0],null,null);
+    clist.push(labCrab);
+    textUpdate();
+};
