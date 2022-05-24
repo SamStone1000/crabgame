77bcacc39fa1693f32ad57369ce134285d36785a
Sam Downs <samstone9001@gmail.com>
2022-05-24 01:36:58 -0400

    add eclipse plugin

--- src/main/java/put/site/crabgame/CrabgameApplication.java
+++ src/main/java/put/site/crabgame/CrabgameApplication.java
@@ -2,12 +2,18 @@ package put.site.crabgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CrabgameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrabgameApplication.class, args);
+	}
+	
+	@GetMapping("/responder")
+	public String responder() {
+		return "This is a response";
 	}
 
 }
